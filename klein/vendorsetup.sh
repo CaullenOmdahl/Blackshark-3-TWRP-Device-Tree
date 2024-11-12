38342e59ec8bad6166dba7d9192105db04a4e9ce
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-13 01:24:40 +0700

    Update vendorsetup.sh

--- klein/vendorsetup.sh
+++ klein/vendorsetup.sh
@@ -1,3 +1,11 @@
-add_lunch_combo orangefox_klein-user
-add_lunch_combo orangefox_klein-userdebug
-add_lunch_combo orangefox_klein-eng
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_LUNCH_CHOICES :=  \
+    orangefox_klein-user \
+    orangefox_klein-userdebug \
+    orangefox_klein-eng
